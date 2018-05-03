ddc8464e64f90643687310e11754e1b7f353c908
Tyler <AT86356@ad2.prod>
2018-05-03 09:41:16 -0400

    created game logic

---
+++ js/main.js
@@ -0,0 +1,8 @@
+console.log("Up and running!");
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queenh";
+var cardFour = "kingh";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+
