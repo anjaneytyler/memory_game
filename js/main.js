cf1045b3303bdd91d30285e99896a8efdbc310d1
Tyler <AT86356@ad2.prod>
2018-05-04 11:54:30 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
 console.log("Up and running!");
+/*
 var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queenh";
 var cardFour = "kingh";
+*/
+
+var cards = ["queen", "queen", "king", "king"]
+var cardsInPlay =["queen", "king"]
+var cardOne = "queen";
+var cardTwo = "king";
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
 
+    if (cardsInPlay === "queen") {
+        console.log("User flipped " + cardOne);
+    }
+    if (cardsInPlay === "king") {
+        console.log("User flipped " + cardTwo);
+    }
+if (cardsInPlay[0] === cardsInPlay[1])  {
+    console.log("You found a match!");
+} else {
+    console.log("Sorry, try again.");
+}
